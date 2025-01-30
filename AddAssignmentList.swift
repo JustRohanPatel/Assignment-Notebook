4aa889034609ba2115632353ef3dd850d5c16038
rohan patel <rpatel5@bsd220.org>
2025-01-30 09:34:22 -0600

    everything works but save

---
+++ AddAssignmentList.swift
@@ -0,0 +1,41 @@
+//
+//  AddAssignmentList.swift
+//  Assignment Notebook
+//
+//  Created by Rohan Patel on 1/29/25.
+//
+
+import SwiftUI
+struct AddAssignmentView: View {
+    @Environment(\.presentationMode) var presentationMode
+    static let courses = ["Civics", "Anatomy", "Precalc", "Semenar", "Spanish", "PLTW", "Mobile App Devolpment"]
+    @ObservedObject var assignmentList: AssignmentList
+    @State private var course = ""
+    @State private var description = ""
+    @State private var dueDate = Date()
+    var body: some View {
+        NavigationView {
+            Form {
+                Picker("Course", selection: $course) {
+                    ForEach(Self.courses, id: \.self) { course in
+                        Text(course)
+                    }
+                }
+                TextField("Description", text: $description)
+                DatePicker ("Due Date", selection: $dueDate, displayedComponents: .date)
+            }
+            .navigationBarTitle("Add New Assignment Item", displayMode: .inline)
+            .navigationBarItems (trailing: Button ("Save") {
+                if course.count > 0 && description.count > 0 {
+                    let item = AssignmentItem(id: UUID(), course: course,
+                                        description: description, dueDate: dueDate)
+                    assignmentList.items.append(item)
+                    presentationMode.wrappedValue.dismiss ()
+                }
+            })
+        }
+    }
+}
+#Preview {
+    AddAssignmentView(assignmentList: AssignmentList())
+}
---
+++ Assignment Notebook/AssignmentList.swift
@@ -0,0 +1,27 @@
+//
+//  AssignmentList.swift
+//  Assignment Notebook
+//
+//  Created by Rohan Patel on 1/29/25.
+//
+
+import Foundation
+class AssignmentList: ObservableObject {
+    @Published var items : [AssignmentItem] {
+        didSet {
+            if let encodedData = try? JSONEncoder() .encode(items) {
+                UserDefaults.standard.set(encodedData, forKey: "data")
+            }
+        }
+    }
+    init() {
+        if let data = UserDefaults.standard.data(forKey: "data") {
+            if let decodedData = try? JSONDecoder().decode([AssignmentItem].self, from: data) {
+                items = decodedData
+                return
+            }
+        }
+        items = []
+    }
+}
+
